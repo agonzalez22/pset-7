6e7d13281d4441cb880802257a0dcb69d1c1dbc4
wiggle <55455721+agonzalez22@users.noreply.github.com>
2020-01-20 21:38:35 -0500

    1

--- src/exercises.js
+++ src/exercises.js
@@ -1,9 +1,17 @@
 function commonEnd(a, b) {
+
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
   }
 
-  // write your code here
+  let lastElementA = a.length - 1;
+  let lastElementB = b.length - 1;
+
+  if (a[0] === b[0] || a[lastElementA] === b[lastElementB]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function endsMeet(values, n) {
