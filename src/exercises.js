43fadd8058aa0583011f86d9803779315037f06d
wiggle <55455721+agonzalez22@users.noreply.github.com>
2020-01-20 21:43:24 -0500

    im also doing my english

--- src/exercises.js
+++ src/exercises.js
@@ -70,7 +70,16 @@ function max(number) {
 }
 
 function middle(values) {
-  // write your code here
+  if (!values || values.length % 2 === 0 || values.length < 3) {
+    return [];
+  } else {
+    let newA = [];
+
+    let middle = Math.floor(values.length / 2);
+    newA.push(values[middle - 1], values[middle], values[middle + 1]);
+
+    return newA;
+  }
 }
 
 function increasing(numbers) {
